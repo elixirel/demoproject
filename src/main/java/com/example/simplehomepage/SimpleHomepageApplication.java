18bbdb2b91d055641c80bb44bd430d36a23ad1f0
elixirel <67626885+elixirel@users.noreply.github.com>
2023-05-15 20:09:04 +0900

    initial commit

---
+++ src/main/java/com/example/simplehomepage/SimpleHomepageApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplehomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHomepageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleHomepageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplehomepage/SimpleHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplehomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHomepageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
